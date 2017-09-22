899e6791886879c77984e00a2f5376b326e833f8
frencius <frenciusleonardu@yahoo.com>
2017-09-22 10:13:58 +0700

    [INITIAL] setting up vue project + several components

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+	<footer>
+		<h2>{{ footer }}</h2>
+	</footer>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				footer: "FOOTER"
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	footer {
+		background: #222;
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
